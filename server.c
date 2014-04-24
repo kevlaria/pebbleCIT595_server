e1fa5541ee7f021b560f2995936fa0c3183a6206
kevlaria <kevlaria@gmail.com>
2014-04-23 21:27:06 -0400

    Minor indentation updates

--- server.c
+++ server.c
@@ -71,45 +71,45 @@ int initial_count_buffer = 0;
 int start_server(int PORT_NUMBER)
 {
 
-      // structs to represent the server and client
-      struct sockaddr_in server_addr,client_addr;    
+    // structs to represent the server and client
+    struct sockaddr_in server_addr,client_addr;    
       
-      int sock; // socket descriptor
+    int sock; // socket descriptor
 
-      // 1. socket: creates a socket descriptor that you later use to make other system calls
-      if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-  perror("Socket");
-  exit(1);
-      }
-      int temp;
-      if (setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&temp,sizeof(int)) == -1) {
-  perror("Setsockopt");
-  exit(1);
-      }
+    // 1. socket: creates a socket descriptor that you later use to make other system calls
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+      perror("Socket");
+      exit(1);
+    }
+    int temp;
+    if (setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&temp,sizeof(int)) == -1) {
+      perror("Setsockopt");
+      exit(1);
+    }
 
-      // configure the server
-      server_addr.sin_port = htons(PORT_NUMBER); // specify port number
-      server_addr.sin_family = AF_INET;         
-      server_addr.sin_addr.s_addr = INADDR_ANY; 
-      bzero(&(server_addr.sin_zero),8); 
+    // configure the server
+    server_addr.sin_port = htons(PORT_NUMBER); // specify port number
+    server_addr.sin_family = AF_INET;         
+    server_addr.sin_addr.s_addr = INADDR_ANY; 
+    bzero(&(server_addr.sin_zero),8); 
       
-      // 2. bind: use the socket and associate it with the port number
-      if (bind(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1) {
-  perror("Unable to bind");
-  exit(1);
-      }
+    // 2. bind: use the socket and associate it with the port number
+    if (bind(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1) {
+      perror("Unable to bind");
+      exit(1);
+    }
 
-      // 3. listen: indicates that we want to listn to the port to which we bound; second arg is number of allowed connections
-      if (listen(sock, 5) == -1) {
-  perror("Listen");
-  exit(1);
-      }
+    // 3. listen: indicates that we want to listn to the port to which we bound; second arg is number of allowed connections
+    if (listen(sock, 5) == -1) {
+      perror("Listen");
+      exit(1);
+    }
           
-      // once you get here, the server is set up and about to start listening
-      printf("\nServer configured to listen on port %d\n", PORT_NUMBER);
-      fflush(stdout);
+    // once you get here, the server is set up and about to start listening
+    printf("\nServer configured to listen on port %d\n", PORT_NUMBER);
+    fflush(stdout);
      
-while(end == 0){
+    while(end == 0){
   
       // 4. accept: wait here until we get a connection on that port
       int sin_size = sizeof(struct sockaddr_in);
@@ -180,16 +180,15 @@ while(end == 0){
 
       else if (strcmp(token, "/standby") == 0){    // Turn on standby
         if(standbyStatus == 1){
-        change_arduino_standby(0);
-        send_success_header("{\n\"status\": \"paused\"\n}");
-        standbyStatus = 0;
-      }
-      else{
-        change_arduino_standby(1);
-        send_success_header("{\n\"status\": \"active\"\n}");
-         standbyStatus = 1;
-      }
-      
+          change_arduino_standby(0);
+          send_success_header("{\n\"status\": \"paused\"\n}");
+          standbyStatus = 0;
+        }
+        else{
+          change_arduino_standby(1);
+          send_success_header("{\n\"status\": \"active\"\n}");
+          standbyStatus = 1;
+        }
           
       }
       
